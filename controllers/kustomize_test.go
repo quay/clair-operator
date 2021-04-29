77188d94fd4384aa6f14a8c41aab6c3f4dad1e77
Hank Donnay <hdonnay@redhat.com>
2021-04-29 12:16:32 -0500

    more templates

--- controllers/kustomize_test.go
+++ controllers/kustomize_test.go
@@ -24,47 +24,64 @@ func TestTemplate(t *testing.T) {
 	})
 	cfg.SetName("injected-cfg")
 
-	tt := []struct {
-		name string
-		mk   func(cfg configObject, img string) (resmap.ResMap, error)
-		img  string
-		gold string
-	}{
+	tt := []templateTestcase{
 		{
-			name: "Indexer",
-			mk:   k.Indexer,
-			img:  "test/image:tag",
-			gold: "testdata/want.indexer.yaml",
+			Name: "Indexer",
+			Mk:   k.Indexer,
+			Img:  "test/image:tag",
+			Want: "testdata/want.indexer.yaml",
+		},
+		{
+			Name: "Matcher",
+			Mk:   k.Matcher,
+			Img:  "test/image:tag",
+			Want: "testdata/want.matcher.yaml",
+		},
+		{
+			Name: "Notifier",
+			Mk:   k.Notifier,
+			Img:  "test/image:tag",
+			Want: "testdata/want.notifier.yaml",
 		},
 	}
 
-	for i := range tt {
-		tc := &tt[i]
-		t.Run(tc.name, func(t *testing.T) {
-			f, err := os.Open(tc.gold)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer f.Close()
-			wantb, err := io.ReadAll(f)
-			if err != nil {
-				t.Fatal(err)
-			}
+	for _, tc := range tt {
+		t.Run(tc.Name, tc.Run(cfg))
+	}
+}
 
-			res, err := tc.mk(cfg, tc.img)
-			if err != nil {
-				t.Error(err)
-			}
-			if res == nil {
-				return
-			}
-			gotb, err := res.AsYaml()
-			if err != nil {
-				t.Error(err)
-			}
-			if got, want := string(gotb), string(wantb); !cmp.Equal(want, got) {
-				t.Error(cmp.Diff(want, got))
-			}
-		})
+type templateTestcase struct {
+	Name string
+	Mk   func(cfg configObject, img string) (resmap.ResMap, error)
+	Img  string
+	Want string
+}
+
+func (tc templateTestcase) Run(cfg *unstructured.Unstructured) func(*testing.T) {
+	return func(t *testing.T) {
+		f, err := os.Open(tc.Want)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		wantb, err := io.ReadAll(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		res, err := tc.Mk(cfg, tc.Img)
+		if err != nil {
+			t.Error(err)
+		}
+		if res == nil {
+			return
+		}
+		gotb, err := res.AsYaml()
+		if err != nil {
+			t.Error(err)
+		}
+		if got, want := string(gotb), string(wantb); !cmp.Equal(want, got) {
+			t.Error(cmp.Diff(want, got))
+		}
 	}
 }
