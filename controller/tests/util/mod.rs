32299b5594ff8093479383ff628482dbd4abad01
Hank Donnay <hdonnay@redhat.com>
2023-06-06 12:45:21 -0500

    add ci xtask
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- controller/tests/util/mod.rs
+++ controller/tests/util/mod.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+/// In_ci reports if the test is being run in CI.
 pub fn in_ci() -> bool {
-    env::var("CI").is_ok()
+    env::var("CI").is_ok_and(|v| v == "true")
 }
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
         .author(crate_authors!())
         .version(crate_version!())
         .about("Build + task support for clair-operator")
-        .subcommand(
+        .subcommand_required(true)
+        .subcommands(&[
             Command::new("bundle")
                 .about("generate OLM bundle")
                 .args(&[
@@ -26,12 +27,13 @@ fn main() {
                     .long_help("CRD patch directory. If unspecified, `api/patch` inside the workspace root will be used.")
                     .value_hint(ValueHint::DirPath),
                 ]),
-        );
+            Command::new("ci").about("run CI setup, then tests"),
+        ]);
 
     if let Err(e) = match cmd.get_matches().subcommand() {
         Some(("bundle", m)) => bundle(crate_version!(), BundleOpts::from(m)),
-        Some((n, _)) => Err(format!("bad subcommand: {n}").into()),
-        None => Err("need subcommand".into()),
+        Some(("ci", _)) => ci(),
+        _ => unreachable!(),
     } {
         eprintln!("{e}");
         process::exit(1);
@@ -40,6 +42,80 @@ fn main() {
 
 type DynError = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, DynError>;
+
+fn ci() -> Result<()> {
+    use std::{env, process::Command};
+    env::set_var("CI", "true");
+    env::set_var("KUBECONFIG", workspace().join("kubeconfig"));
+    let _guard = KIND::new()?;
+    let _ = Command::new("kubectl")
+        .args(&[
+            "wait",
+            "pods",
+            "--for=condition=Ready",
+            "--timeout=300s",
+            "--all",
+            "--all-namespaces",
+        ])
+        .status();
+    let _ = Command::new("kubectl")
+        .args(&[
+            "label",
+            "namespace",
+            "default",
+            "clair.projectquay.io/safe-to-run-tests=true",
+        ])
+        .status();
+    let status = Command::new(env::var_os("CARGO").unwrap())
+        .args(&["test"])
+        .current_dir(workspace())
+        .status()?;
+    if !status.success() {
+        return Err("tests failed".into());
+    }
+    Ok(())
+}
+
+struct KIND {
+    name: std::ffi::OsString,
+}
+impl Drop for KIND {
+    fn drop(&mut self) {
+        use std::process::Command;
+        let mut cmd = Command::new("kind");
+        cmd.current_dir(workspace());
+        cmd.arg("delete");
+        cmd.arg("cluster");
+        cmd.arg("--name");
+        cmd.arg(&self.name);
+        let _ = cmd.status();
+    }
+}
+impl KIND {
+    fn new() -> Result<Self> {
+        use std::process::Command;
+        let k8s_ver = std::env::var("KUBE_VERSION").unwrap_or(String::from("1.25"));
+        let kind_name = "ci";
+        let kind_config = workspace()
+            .join("controller/etc/tests/")
+            .join(format!("kind-{}.yaml", k8s_ver));
+        let mut cmd = Command::new("kind");
+        cmd.current_dir(workspace());
+        cmd.arg("--config");
+        cmd.arg(&kind_config);
+        cmd.arg("create");
+        cmd.arg("cluster");
+        cmd.arg("--name");
+        cmd.arg(&kind_name);
+        let status = cmd.status()?;
+        if !status.success() {
+            return Err("kind exit non-zero".into());
+        }
+        Ok(Self {
+            name: kind_name.into(),
+        })
+    }
+}
 
 fn bundle(v: &str, opts: BundleOpts) -> Result<()> {
     use api::v1alpha1;
