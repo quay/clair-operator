125836d2f0bd45fa721c706a7f5ac5d12404d034
Hank Donnay <hdonnay@redhat.com>
2023-06-20 14:16:17 -0500

    clair_config: remove kube usage from clair_config
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- clair_config/src/lib.rs
+++ clair_config/src/lib.rs
@@ -7,8 +7,9 @@
 #![warn(rustdoc::missing_crate_level_docs)]
 #![warn(missing_docs)]
 
+use std::collections::BTreeMap;
+
 use k8s_openapi::api::core;
-use kube::{api::Api, Client};
 use tracing::{debug, trace};
 
 use api::v1alpha1;
@@ -29,8 +30,6 @@ pub enum Error {
     JSON(serde_json::Error),
     /// JSON Patch error
     Patch(json_patch::PatchError),
-    /// Generic k8s access error.
-    Kube(kube::Error),
 
     /// Error for testing only.
     #[cfg(test)]
@@ -67,11 +66,6 @@ impl From<json_patch::PatchError> for Error {
         Self::Patch(err)
     }
 }
-impl From<kube::Error> for Error {
-    fn from(err: kube::Error) -> Self {
-        Self::Kube(err)
-    }
-}
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -80,7 +74,6 @@ impl std::fmt::Display for Error {
             Error::YAML(err) => write!(f, "YAML error: {err}"),
             Error::JSON(err) => write!(f, "JSON error: {err}"),
             Error::Patch(err) => write!(f, "json patch error: {err}"),
-            Error::Kube(err) => write!(f, "k8s client error: {err}"),
             #[cfg(test)]
             Error::Test(msg) => write!(f, "testing error: {msg}"),
         }
@@ -89,75 +82,158 @@ impl std::fmt::Display for Error {
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
-/// Validate calls into [`config.Validate`] and reports the lints for every mode.
-/// This is done by composing the config in-process accoring to the [`cmd.Load`] documentation.
-/// The changes for defaults made by the `Validate` function are not returned, so that the config
-/// package can change the defaults as needed.
-///
-/// [`config.Validate`]: https://pkg.go.dev/github.com/quay/clair/config#Validate
-/// [`cmd.Load`]: https://pkg.go.dev/github.com/quay/clair/v4/cmd#Load
-pub async fn validate(client: &Client, cfg: &v1alpha1::ConfigSource) -> Result<Validate> {
-    let flavor = match &cfg.root.key.rsplit_once('.') {
-        Some((_, ext)) => match ext {
-            &"json" => v1alpha1::ConfigDialect::JSON,
-            &"yaml" => v1alpha1::ConfigDialect::YAML,
-            ext => return Err(Error::invalid(format!("unknown file extension: {ext}"))),
-        },
-        None => return Err(Error::invalid("missing file extension")),
-    };
-    debug!(?flavor, "config flavor from root name");
-    let (doc, _) = load_config(client, Source::ConfigMap(&cfg.root)).await?;
-    let doc = to_json(doc, &flavor).await?;
-    let mut doc: serde_json::Value = serde_json::from_slice(&doc)?;
+/// Parts is the loaded parts of a Clair config, converted to JSON form.
+pub struct Parts {
+    root: Vec<u8>,
+    dropins: BTreeMap<String, (Vec<u8>, bool)>,
+}
 
-    assert!(cfg
-        .dropins
-        .iter()
-        .all(|c| c.secret_key_ref.is_some() || c.config_map_key_ref.is_some()));
+impl Parts {
+    /// Validate calls into [`config.Validate`] and reports the lints for every mode.
+    /// This is done by composing the config in-process accoring to the [`cmd.Load`] documentation.
+    /// The changes for defaults made by the `Validate` function are not returned, so that the config
+    /// package can change the defaults as needed.
+    ///
+    /// [`config.Validate`]: https://pkg.go.dev/github.com/quay/clair/config#Validate
+    /// [`cmd.Load`]: https://pkg.go.dev/github.com/quay/clair/v4/cmd#Load
+    pub async fn validate(&self) -> Result<Validate> {
+        let doc = serde_json::from_slice(&self.root)?;
+        let doc = self
+            .dropins
+            .iter()
+            .fold(doc, |mut doc, (name, (buf, patch))| {
+                if *patch {
+                    let p: json_patch::Patch =
+                        serde_json::from_slice(&buf).expect("failed to load patch");
+                    trace!(name, "applying patch");
+                    json_patch::patch(&mut doc, &p).expect("failed to apply patch");
+                } else {
+                    let m: serde_json::Value =
+                        serde_json::from_slice(&buf).expect("failed to parse JSON");
+                    trace!(name, "merging config");
+                    json_patch::merge(&mut doc, &m);
+                };
+                doc
+            });
+        trace!("config rendered");
+        let doc = serde_json::to_vec(&doc)?;
+        Ok(Validate {
+            indexer: validate_config(&doc, "indexer").await,
+            matcher: validate_config(&doc, "matcher").await,
+            notifier: validate_config(&doc, "notifier").await,
 
-    let mut order = cfg
-        .dropins
-        .iter()
-        .enumerate()
-        .map(|(i, v)| {
-            let key = if let Some(cmref) = &v.config_map_key_ref.as_ref() {
-                &cmref.key
-            } else if let Some(secref) = &v.secret_key_ref.as_ref() {
-                &secref.key
-            } else {
-                unreachable!()
-            };
-            (i, key)
+            updater: validate_config(&doc, "updater").await,
         })
-        .collect::<Vec<_>>();
-    order.sort_by_key(|(_, k)| k.as_str());
-    for (i, _) in order {
-        let r = &cfg.dropins[i];
-        let src = if let Some(cmref) = &r.config_map_key_ref {
-            Source::ConfigMap(cmref)
-        } else if let Some(secref) = &r.secret_key_ref {
-            Source::Secret(secref)
-        } else {
-            unreachable!()
-        };
-        let (buf, is_patch) = load_config(client, src).await?;
-        let buf = to_json(buf, &flavor).await?;
-        if is_patch {
-            let p: json_patch::Patch = serde_json::from_slice(&buf)?;
-            json_patch::patch(&mut doc, &p)?;
-        } else {
-            let m: serde_json::Value = serde_json::from_slice(&buf)?;
-            json_patch::merge(&mut doc, &m);
+    }
+}
+
+impl From<Builder> for Parts {
+    fn from(b: Builder) -> Self {
+        Self {
+            root: b.root,
+            dropins: b.dropins,
         }
     }
+}
 
-    let doc = serde_json::to_vec(&doc)?;
-    Ok(Validate {
-        indexer: validate_config(&doc, "indexer").await,
-        matcher: validate_config(&doc, "matcher").await,
-        updater: validate_config(&doc, "updater").await,
-        notifier: validate_config(&doc, "notifier").await,
-    })
+/// Builder constructs all the root and dropins for a configuration.
+pub struct Builder {
+    flavor: v1alpha1::ConfigDialect,
+
+    root: Vec<u8>,
+    dropins: BTreeMap<String, (Vec<u8>, bool)>,
+}
+
+impl Builder {
+    /// From_root constructs a Builder starting with the root config.
+    pub fn from_root<S: ToString>(v: &core::v1::ConfigMap, key: S) -> Result<Self> {
+        let key = key.to_string();
+        let root = if let Some(map) = &v.data {
+            map.get(&key).map(|s| s.clone().into_bytes())
+        } else if let Some(map) = &v.binary_data {
+            map.get(&key).map(|b| b.0.clone())
+        } else {
+            unreachable!()
+        }
+        .ok_or_else(|| Error::invalid(format!("missing key: {key}")))?;
+        trace!(key, "loaded key");
+        let flavor = match key.rsplit_once('.') {
+            Some((_, ext)) => match ext {
+                "json" => v1alpha1::ConfigDialect::JSON,
+                "yaml" => v1alpha1::ConfigDialect::YAML,
+                ext => return Err(Error::invalid(format!("unknown file extension: {ext}"))),
+            },
+            None => return Err(Error::invalid("missing file extension")),
+        };
+        trace!(%flavor, "guessed config flavor");
+        let root = to_json(root, &flavor)?;
+        trace!(key, "converted to JSON");
+        debug!("created Builder");
+        Ok(Builder {
+            flavor,
+            root,
+            dropins: Default::default(),
+        })
+    }
+
+    /// Add adds a dropin, converting to JSON if needed.
+    pub fn add<M, S>(mut self, map: M, key: S) -> Result<Self>
+    where
+        M: K8sMap,
+        S: ToString,
+    {
+        let key = key.to_string();
+        let is_patch = key.ends_with("-patch");
+        let buf = map
+            .value(key.clone())
+            .ok_or_else(|| Error::invalid(format!("missing key: {key}")))?;
+        trace!(key, is_patch, "loaded key");
+        let buf = to_json(buf, &self.flavor)?;
+        trace!(key, is_patch, "converted to JSON");
+        self.dropins.insert(key, (buf, is_patch));
+        debug!("added dropin");
+        Ok(self)
+    }
+}
+
+mod private {
+    pub trait Sealed {}
+}
+use private::Sealed;
+
+/// K8sMap is a k8s map-type: a ConfigMap or a Secret.
+pub trait K8sMap: Sealed {
+    /// Value returns the value for the key.
+    fn value(&self, key: String) -> Option<Vec<u8>>;
+}
+
+impl Sealed for core::v1::ConfigMap {}
+impl K8sMap for core::v1::ConfigMap {
+    fn value(&self, key: String) -> Option<Vec<u8>> {
+        if let Some(data) = &self.data {
+            if let Some(buf) = data.get(&key) {
+                return Some(buf.clone().into_bytes());
+            };
+        };
+        if let Some(data) = &self.binary_data {
+            if let Some(buf) = data.get(&key) {
+                return Some(buf.0.clone());
+            };
+        };
+        None
+    }
+}
+
+impl Sealed for core::v1::Secret {}
+impl K8sMap for core::v1::Secret {
+    fn value(&self, key: String) -> Option<Vec<u8>> {
+        if let Some(data) = &self.data {
+            if let Some(buf) = data.get(&key) {
+                return Some(buf.0.clone());
+            };
+        };
+        None
+    }
 }
 
 /// Validate reports results for all the Clair operating modes.
@@ -212,7 +288,7 @@ impl std::fmt::Debug for Warnings {
 }
 
 /// To_json returns the bytes jsonified.
-async fn to_json(buf: Vec<u8>, flavor: &v1alpha1::ConfigDialect) -> Result<Vec<u8>> {
+fn to_json(buf: Vec<u8>, flavor: &v1alpha1::ConfigDialect) -> Result<Vec<u8>> {
     match flavor {
         v1alpha1::ConfigDialect::JSON => Ok(buf),
         v1alpha1::ConfigDialect::YAML => {
@@ -222,53 +298,12 @@ async fn to_json(buf: Vec<u8>, flavor: &v1alpha1::ConfigDialect) -> Result<Vec<u
     }
 }
 
-/// Source is a discriminator between a ConfigMap and a Secret.
-enum Source<'a> {
-    ConfigMap(&'a v1alpha1::ConfigMapKeySelector),
-    Secret(&'a v1alpha1::SecretKeySelector),
-}
-
-/// Load_config returns the bytes of the referenced config and whether it should be interpreted as
-/// a patch or not.
-async fn load_config(client: &Client, config_ref: Source<'_>) -> Result<(Vec<u8>, bool)> {
-    trace!("loading config");
-    let maps = match config_ref {
-        Source::ConfigMap(r) => {
-            trace!(name = r.name, key = r.key, "checking ConfigMap");
-            let api: Api<core::v1::ConfigMap> = Api::default_namespaced(client.clone());
-            let cm = api.get(&r.name).await?;
-            (cm.data, cm.binary_data)
-        }
-        Source::Secret(r) => {
-            trace!(name = r.name, key = r.key, "checking Secret");
-            let api: Api<core::v1::Secret> = Api::default_namespaced(client.clone());
-            let s = api.get(&r.name).await?;
-            (None, s.data)
-        }
-    };
-    let key = match config_ref {
-        Source::ConfigMap(r) => &r.key,
-        Source::Secret(r) => &r.key,
-    };
-    let is_patch = key.ends_with("-patch");
-    trace!(is_patch, strdata=?maps.0, bindata=?maps.1, "checking keys");
-    if let Some(map) = maps.0 {
-        if let Some(v) = map.get(key) {
-            return Ok((Vec::from(v.as_str()), is_patch));
-        }
-    }
-    if let Some(map) = maps.1 {
-        if let Some(v) = map.get(key) {
-            return Ok((v.0.clone(), is_patch));
-        }
-    }
-
-    Err(Error::invalid("missing key"))
-}
-
-// Validate_config wraps a call to [config.Validate].
-//
-// [config.Validate]: https://pkg.go.dev/github.com/quay/clair/config#Validate
+/// Validate_config wraps a call to [config.Validate].
+///
+/// The use of `block_in_place` here means we have a depenedency on tokio, but should make the ffi
+/// play nicer with the multi-threaded runtime (in theory).
+///
+/// [config.Validate]: https://pkg.go.dev/github.com/quay/clair/config#Validate
 async fn validate_config<S: AsRef<str>>(buf: &[u8], mode: S) -> Result<Warnings> {
     use libc::free;
     use std::ffi::{self, CStr};
